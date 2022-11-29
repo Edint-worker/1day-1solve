4c220a291ad553103f1156b9dd6770d12352ac55
unchaptered <workstation19961002@gmail.com>
2022-11-29 13:59:10 +0900

    Update READMEgenerator

--- README-generator.js
+++ README-generator.js
@@ -90,8 +90,8 @@ function addHistoryLine(fileList, fileKeys, startDay, lastDay) {
             const fileName = filteredValue.map(v => v.join('-'));
             les[fileKey] = filteredValue.map(v => {
 
-                const [l, v, n] = fileKey;
-                const tempKey = l + v + '-' + n;
+                const [firstWord, secondWord, lastWord] = fileKey;
+                const tempKey = firstWord + secondWord + '-' + lastWord;
                 const path = `https://github.com/Edint-worker/1day-1solve/blob/main/${tempKey}/${v.join('-')}`;
                 const encodedPath = encodeURI(path);
                 return `[${v[1]}-${v[2]}](${encodedPath})`;
